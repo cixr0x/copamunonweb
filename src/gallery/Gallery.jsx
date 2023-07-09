1a5b6d6fe8a7d87522d918bb30c1dcaf9c016502
robert <robertorojasmo@gmail.com>
2023-07-09 17:31:48 -0600

    tesing twitch embed

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -9,6 +9,7 @@ function Gallery() {
         <div className="calendar-cards-container">
         <h1>GALERÍA</h1>
         </div>
+        <div><iframe src="https://player.twitch.tv/?video=1862812795&parent=www.copamunon.com" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe></div>
         </>
     )
 }
