58e253b1de4f8fe0fb298ab12279f7f5e9715969
robert <robertorojasmo@gmail.com>
2024-01-21 04:02:51 -0600

    Removed reserve from dotd voting

--- src/driveroftheday/Driveroftheday.jsx
+++ src/driveroftheday/Driveroftheday.jsx
@@ -52,6 +52,7 @@ function Drivers(props) {
         }
 
         for (let data of tableData) {
+            if (data.driver.role === "reserva") continue;
             table.push(
                 <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 margin-bot" >
                     <Card
